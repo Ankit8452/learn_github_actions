ddd19a08331d290d51650ef68d6145d071b89d67
Ankit8452 <ankit.dubey@yellowmatrix.in>
2025-09-09 19:38:06 +0530

    Version 2

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return jsonify({"message" : "Learning GitHub Action"})
 
+@app.route('/githubactions')
+def action():
+    return jsonify({"message" : "Github Action Code running successfully"})
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
