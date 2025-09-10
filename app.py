16b4c813e535040384425dac830adbc8c1fd5ef2
Ankit8452 <ankit.dubey@yellowmatrix.in>
2025-09-10 17:48:06 +0530

    Private Repo

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"message" : "Learning GitHub Action"})
+    return jsonify({"message" : "Learning GitHub Action"}), 200
 
 @app.route('/githubactions')
 def action():
-    return jsonify({"message" : "Github Action Code running successfully"})
+    return jsonify({"message" : "Github Action Code running successfully"}), 200
+
+@app.route('testprivaterepo')
+def testPull():
+    return jsonify({"message" : "Pulling the Private Repo"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
